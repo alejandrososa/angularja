b39d2df435677fd77db8ba550c4edef3a67afe22
Alejandro Sosa <elpastorsalomon@gmail.com>
2015-10-07 22:09:27 +0200

    Tabla de usuarios

--- secciones/oficina/usuarios/usuarios.ctrl.js
+++ secciones/oficina/usuarios/usuarios.ctrl.js
@@ -65,76 +65,47 @@ angular
             pais: _datos.pais
         };
 
-        vm.columns = [{
+        vm.columns = [
+            /*{
             title: '',
             field: 'thumb',
             sortable: false,
-            filter: 'tableImage'
-        },{
-            title: 'Name',
-            field: 'name',
-            sortable: true
-        },{
-            title: 'Description',
-            field: 'description',
-            sortable: true
-        },{
-            title: 'Date of Birth',
-            field: 'birth',
-            sortable: true
-        }];
+            //filter: 'tableImage'
+            },*/{
+                title: 'Id',
+                field: 'id',
+                sortable: true
+            },{
+                title: 'Usuario',
+                field: 'usuario',
+                sortable: true
+            },{
+                title: 'Correo',
+                field: 'correo',
+                sortable: true
+            },{
+                title: 'Telefono',
+                field: 'tel',
+                sortable: true
+            },{
+                title: 'Fecha',
+                field: 'fecha',
+                sortable: true
+            },];
 
-        vm.contents = [{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-1.png',
-            name: 'Chris Doe',
-            description: 'Developer',
-            birth: 'Jun 5, 1994'
-        },{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-2.png',
-            name: 'Ann Doe',
-            description: 'Commerce',
-            birth: 'Jul 15, 1993'
-        },{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-3.png',
-            name: 'Mark Ronson',
-            description: 'Designer',
-            birth: 'Jan 27, 1984'
-        },{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-4.png',
-            name: 'Eric Doe',
-            description: 'Human Resources',
-            birth: 'Feb 3, 1985'
-        },{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-5.png',
-            name: 'John Doe',
-            description: 'Commerce',
-            birth: 'Sep 5, 1978'
-        },{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-1.png',
-            name: 'George Doe',
-            description: 'Media',
-            birth: 'Jun 23, 1996'
-        },{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-2.png',
-            name: 'Ann Ronson',
-            description: 'Commerce',
-            birth: 'Aug 16, 1995'
-        },{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-3.png',
-            name: 'Adam Ronson',
-            description: 'Developer',
-            birth: 'Jan 7, 1987'
-        },{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-4.png',
-            name: 'Hansel Doe',
-            description: 'Social Media',
-            birth: 'Feb 13, 1977'
-        },{
-            thumb:'assets/plantillas/admin/img/avatars/avatar-5.png',
-            name: 'Tony Doe',
-            description: 'CEO',
-            birth: 'Sep 29, 1970'
-        }];
+        vm.contenido = [];
+
+        angular.forEach(vm.usuarios, function(d){
+
+            vm.contenido.push({
+                id: d.id,
+                usuario: d.usuario,
+                correo: d.correo,
+                tel: d.telefono,
+                fecha: d.fechacreado
+            });
+
+        });
 
 
         //dataprovider
